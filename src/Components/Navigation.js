722d78e508d472c30b1d9146306588b5ccdfe815
Kaldray <jordray@protonmail.com>
2021-07-18 21:21:58 +0200

    Navigation on

---
+++ src/Components/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navigation">
+          <a href="#">Accueil</a>
+          <a href="#">Projets</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
